1401f7c845abaf81944ce04e9b5e61c30e2de9f5
NasirYousuf <102212241+Nasir-Yousuf@users.noreply.github.com>
2022-08-19 20:57:39 +0600

    index.js file added

---
+++ index.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+const Todo = () => {
+  const [todo, setTodo] = useState([]);
+  const [input, setInput] = useState('');
+
+  const onInputChange = (e) => {
+    setInput(e.target.value);
+  };
+  const addTodo = () => {
+    setTodo([...todo, input]);
+  };
+
+  const deleteBtn = (i) => {
+    todo.splice(i, 1);
+  };
+
+  return (
+    <div>
+      <input type="text" onChange={(e) => onInputChange(e)} value={input} />
+      <button onClick={addTodo}>Add Todo</button>
+      <ul>
+        {todo.map((todo, i) => (
+          <li key={todo}>
+            {todo} <button onClick={(i) => deleteBtn(i)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Todo;
